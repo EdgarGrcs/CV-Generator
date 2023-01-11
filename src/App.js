6e5c50bdb019be1e4e2e5bffedfcba5e8078dc5a
EdgarGrcs <edgar.gricius2@gmail.com>
2023-01-11 13:09:13 +0100

    initial commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+
+
+/**
+ * Goals:
+ * website that makes my life easier in creating / editing a CV
+ * Make it possible to download it as PDF
+ * optional: possibility of uploading word doc / pdf and editing it.
+ */
+
+
+function App() {
+  return (
+    <div className="App">
+    </div>
+  );
+}
+
+export default App;
